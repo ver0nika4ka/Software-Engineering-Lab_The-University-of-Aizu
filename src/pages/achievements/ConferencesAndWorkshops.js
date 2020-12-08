37880e8efb18e895736a07a6c3202f833e1c8cad
mk668a <msftoaa@gmail.com>
2020-12-09 00:41:45 +0900

    connect ja and en article

--- src/pages/achievements/ConferencesAndWorkshops.js
+++ src/pages/achievements/ConferencesAndWorkshops.js
@@ -13,18 +13,19 @@ function ConferencesAndWorkshops() {
   useEffect(() => {
     let baseUrl = 'https://software-engineering-lab.microcms.io/api/v1/posts'
     // select article (ja or en)
-    let articleId = (i18n.language === 'ja') ? 'jpspbdfmj' : '3aoa2f1c8o'
+    let articleId = 'ConferencesAndWorkshops'
     // get API-KEY from .env
     const config = { 'X-API-KEY': process.env.REACT_APP_API_KEY };
     // get request
     axios.get(`${baseUrl}/${articleId}`, { headers: config })
       .then(response => {
+        // console.log(response.data);
         setArticle(response.data)
       })
       .catch(error => {
         console.log(error);
       })
-  }, [i18n.language]);
+  }, []);
 
   return (
     <div className="ConferencesAndWorkshops">
@@ -32,9 +33,9 @@ function ConferencesAndWorkshops() {
         <div className="container_contentFrame">
           {article &&
             <>
-              <h1 className="container_title">{article.title}</h1>
+              <h1 className="container_title">{i18n.language === 'ja' ? article.title_ja : article.title_en}</h1>
               <div className="article">
-                <div dangerouslySetInnerHTML={{ __html: article.content }}></div>
+                <div dangerouslySetInnerHTML={{ __html: i18n.language === 'ja' ? article.content_ja : article.content_en }}></div>
               </div>
             </>
           }
