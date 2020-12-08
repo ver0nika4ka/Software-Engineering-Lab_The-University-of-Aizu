6e8babb0b9c283b58c55802aa846e32cd64a2bd9
mk668a <msftoaa@gmail.com>
2020-12-09 01:16:06 +0900

    [Publications] get article

--- src/pages/publication_list/Publications.js
+++ src/pages/publication_list/Publications.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 // i18n
 import { useTranslation } from 'react-i18next';
 // images
@@ -26,25 +27,51 @@ function Publications() {
     link: "https://www.u-aizu.ac.jp/research/faculty/detail?cd=90072"
   }]
 
+  // *************** get article data *************** //
+  const [article, setArticle] = useState();
+
+  // If the language changes, this API will be re-executed
+  useEffect(() => {
+    let baseUrl = 'https://software-engineering-lab.microcms.io/api/v1/posts'
+    // select article (ja or en)
+    let articleId = 'Publications'
+    // get API-KEY from .env
+    const config = { 'X-API-KEY': process.env.REACT_APP_API_KEY };
+    // get request
+    axios.get(`${baseUrl}/${articleId}`, { headers: config })
+      .then(response => {
+        // console.log(response.data);
+        setArticle(response.data)
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }, []);
+
   return (
     <div className="Publications">
       <div className="container_content">
-        <h2 className="container_title">{t('指導教員')}</h2>
-        <div className="Publications_professors flex center">
-          {professors.map((professor, i) => (
-            <div className="Publications_professor" key={i}>
-              <a href={professor.link} target="_blank" className="flex wrap" rel="noopener noreferrer" >
-                <img className="Publications_professorImage " src={professor.photo} alt="" />
-                <p className="Publications_professorName ">{t(professor.name)}</p>
-              </a>
-            </div>
-          ))}
+        <div className="container_contentFrame">
+          <h2 className="container_title">{t('指導教員')}</h2>
+          <div className="Publications_professors flex center">
+            {professors.map((professor, i) => (
+              <div className="Publications_professor" key={i}>
+                <a href={professor.link} target="_blank" className="flex wrap" rel="noopener noreferrer" >
+                  <img className="Publications_professorImage " src={professor.photo} alt="" />
+                  <p className="Publications_professorName ">{t(professor.name)}</p>
+                </a>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-
-      <div className="container_content">
-        <h2 className="container_title">{t('学生による論文')}</h2>
-        {/* TODO: HeadressCMS */}
+        {article &&
+          <div className="container_contentFrame">
+            <h2 className="container_title">{i18n.language === 'ja' ? article.title_ja : article.title_en}</h2>
+            <div className="article">
+              <div dangerouslySetInnerHTML={{ __html: i18n.language === 'ja' ? article.content_ja : article.content_en }}></div>
+            </div>
+          </div>
+        }
       </div>
     </div>
   );
