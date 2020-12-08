30e88427de6277ea80e298988444f7d0f1b3f0f0
mk668a <msftoaa@gmail.com>
2020-12-09 00:52:04 +0900

    [Achievements] get article

--- src/pages/achievements/Achievements.js
+++ src/pages/achievements/Achievements.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 // i18n
 import { useTranslation } from 'react-i18next';
 
 function Achievements() {
   const [t, i18n] = useTranslation();
 
+  // *************** get article data *************** //
+  const [article, setArticle] = useState();
+
+  // If the language changes, this API will be re-executed
+  useEffect(() => {
+    let baseUrl = 'https://software-engineering-lab.microcms.io/api/v1/posts'
+    // select article (ja or en)
+    let articleId = 'Achievements'
+    // get API-KEY from .env
+    const config = { 'X-API-KEY': process.env.REACT_APP_API_KEY };
+    // get request
+    axios.get(`${baseUrl}/${articleId}`, { headers: config })
+      .then(response => {
+        // console.log(response.data);
+        setArticle(response.data)
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }, []);
+
   return (
     <div className="Achievements">
       <div className="container_content">
         <div className="container_contentFrame">
-          <h2 className="container_title">{t('業績')}</h2>
-          <h3 className="container_subtitle">{t('受賞')}</h3>
-          <h3 className="container_subtitle">{t('研究業績')}</h3>
+          {article &&
+            <>
+              <h1 className="container_title">{i18n.language === 'ja' ? article.title_ja : article.title_en}</h1>
+              <div className="article">
+                <div dangerouslySetInnerHTML={{ __html: i18n.language === 'ja' ? article.content_ja : article.content_en }}></div>
+              </div>
+            </>
+          }
         </div>
       </div>
     </div>
