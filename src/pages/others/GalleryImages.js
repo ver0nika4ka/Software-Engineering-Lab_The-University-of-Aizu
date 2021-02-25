02f1128c4901449751e0ac3ed5d58bd2ea2737ae
Veronica <veranika.aizu@gmail.com>
2021-02-25 15:56:03 +0900

    change gallery name and add carousel to gallery

--- src/pages/others/GalleryImages.js
+++ src/pages/others/GalleryImages.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry"
+import { Carousel } from 'react-responsive-carousel';
 // i18n
 import { useTranslation } from 'react-i18next';
 // styles
@@ -23,6 +24,15 @@ function GalleryImages({ galleryName, galleryImages }) {
     setModalImage({ name: name, num: num })
   }
 
+  const renameGalleryTitle = (str, tag)=>{
+     let name = ""
+    const regex = /[A-Z][A-Z]+|[A-Z][a-z]+|\d+/g;
+              str.match(regex).forEach(c => {
+                name += `<${tag}>${c}&nbsp;</${tag}>`
+              });
+              return name
+  }
+
   return (
     <div className="GalleryImages">
       <div className="GalleryImagesWrapper">
@@ -31,18 +41,18 @@ function GalleryImages({ galleryName, galleryImages }) {
           <div className="galleryItems flex wrap">
             {galleryImages.map((g, i) => {
               // Line feed setting
-              let name = ""
-              const regex = /[A-Z][A-Z]+|[A-Z][a-z]+|\d+/g;
-              g.name.match(regex).forEach(c => {
-                name += `<p>${c}</p>`
-              });
+              // let name = ""
+              // const regex = /[A-Z][A-Z]+|[A-Z][a-z]+|\d+/g;
+              // g.name.match(regex).forEach(c => {
+              //   name += `<p>${c}&nbsp;</p>`
+              // });
 
               return (
                 <div key={g.name} className="galleryItem"
                   style={{ backgroundImage: `url(${require(`../../images/gallery/${g.name}/1.jpg`)})` }}
                   onClick={() => changeOpenItem(i)}>
                   <div className="galleryItemInfo">
-                    <div className="galleryItemInfo_name" dangerouslySetInnerHTML={{ __html: name }}></div>
+                    <div className="galleryItemInfo_name" dangerouslySetInnerHTML={{ __html: renameGalleryTitle(g.name,"p") }}></div>
                     <div className="galleryItemInfo_imageNum"><p>{g.imageNum}</p></div>
                   </div>
                 </div>
@@ -53,8 +63,9 @@ function GalleryImages({ galleryName, galleryImages }) {
 
         {openItem !== null && galleryImages[openItem] &&
           <div className="galleryItemContents">
-            <div className="galleryItemContents_header flex space-between">
-              <h4 className="galleryItemContents_title">{t(galleryName)} {'>'} {galleryImages[openItem].name}</h4>
+            <div className="galleryItemContents_header flex flex-start">
+              <h4 className="galleryItemContents_title">{t(galleryName)}&nbsp;{'>'}&nbsp;</h4>
+              <h4 className="galleryItemContents_title flex" dangerouslySetInnerHTML={{ __html: renameGalleryTitle(galleryImages[openItem].name, "h4") }}></h4>
               <button className="galleryItemContents_backButton" onClick={() => {
                 setOpenItem(null)
               }}>{'<'}</button>
@@ -87,7 +98,24 @@ function GalleryImages({ galleryName, galleryImages }) {
               setModalImage(null)
             }}>
             </div>
-            <img src={require(`../../images/gallery/${modalImage.name}/Original/${modalImage.num}.jpg`)}></img>
+             {(() => {
+              const items = [];
+              for (let j = 0; j < galleryImages[openItem].imageNum; j++) {
+                items.push(<img key={j} src={require(`../../images/gallery/${galleryImages[openItem].name}/Original/${j + 1}.jpg`)}/>)
+              }
+              return (
+                <div className="galleryImages">
+                  <ResponsiveMasonry columnsCountBreakPoints={{ 300: 2, 400: 3, 500: 4, 700: 5 }}>
+                    <Masonry>
+                        <Carousel infiniteLoop autoPlay showStatus={false} showThumbs={false} stopOnHover>
+                      {items}
+                        </Carousel>
+                    </Masonry>
+                  </ResponsiveMasonry>
+                </div>
+              )
+            })()}
+
           </div>
         }
 
